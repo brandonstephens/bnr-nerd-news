abcb9ba7ce7949a4d65128513c315ce5670340ea
Brandon Stephens <brandonstephens@me.com>
2015-12-03 21:33:44 -0500

    scoped models

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,8 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :commentalbe, polymorphic: true
+
+  validates :user, :commentable, :body, presence: true
+
+  scope :latest, -> { order(created_at: :desc) }
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,6 @@
 class Post < ActiveRecord::Base
   belongs_to :user
+  has_many :comments, as: :commentable
+
+  scope :latest, -> { order(created_at: :desc) }
 end
